24781de36fde4173788f85fa95f94beb64c74b55
jacob-harris <jacob.gimbert@education.nsw.gov.au>
2015-03-12 14:08:13 +1100

    initial commit

---
+++ Module1.vb
@@ -0,0 +1,101 @@
+﻿Module Module1
+    Class information
+        Public companyname As String
+        Public ownersname As String
+        Public contactnumber As Integer
+        Public address As String
+        Public hourlyrate As Single
+        Public hourscompleted As Integer
+
+
+
+    End Class
+    Dim company As New information
+
+
+
+
+
+
+    Sub Main()
+
+        Console.Clear()
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.BackgroundColor = ConsoleColor.Green
+
+        Console.Clear()
+        Console.WriteLine("Welcome to the fun wit lawns v0.1 ")
+        Console.WriteLine(" your all in one lawn management system.")
+        Console.WriteLine()
+        Console.WriteLine(" press any key to continue ....")
+
+
+        Console.ReadLine()
+
+    End Sub
+    Sub noprofile()
+
+        Console.Clear()
+        Console.ForegroundColor = ConsoleColor.Black
+        Console.BackgroundColor = ConsoleColor.Green
+
+
+        Console.WriteLine("no company information has been found. we'll set up a profile before we begin ")
+        Console.WriteLine()
+        Console.WriteLine("press any key to continue...")
+        Console.ReadLine()
+
+        details()
+    End Sub
+
+    Sub details()
+
+
+
+        Console.WriteLine("here you need to enter the details for ypur new company profile. ")
+        Console.WriteLine()
+        Console.Write("company name: ")
+        company.companyname = Console.ReadLine
+        Console.Write("company owner's name: ")
+        company.ownersname = Console.ReadLine
+        Console.Write("company contact number: ")
+        company.contactnumber = Console.ReadLine()
+        Console.Write("company address: ")
+        company.address = Console.ReadLine
+        Console.Write("company hourly rate: ")
+        company.hourlyrate = Console.ReadLine()
+        Console.WriteLine()
+        Console.WriteLine("setup is complete! ")
+        Console.WriteLine()
+        Console.WriteLine("press any key to continue... ")
+
+    End Sub
+    Sub menu()
+
+        Console.WriteLine("Welcome")
+
+
+
+
+
+
+
+    End Sub
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("fun with lawns")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("NSW-DET")> 
+<Assembly: AssemblyProduct("fun with lawns")> 
+<Assembly: AssemblyCopyright("Copyright © NSW-DET 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b5d0a998-e1f9-475c-aaad-24307e16daff")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
